3e9f4526cc5e56f84b54e3ce2d267d969c4a72be
lucas <lucasbortolisantos@gmail.com>
2024-04-15 08:37:30 -0300

    Adicionar botões novos na tela inicial para controle do firmware

---
+++ mobile_interface/app/views/ParameterSetup/GroupBox/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from "react-native";
+import { Text } from "react-native-paper";
+import { PropsWithChildren } from "react";
+
+interface GroupBoxProps extends PropsWithChildren {
+  title: string;
+}
+
+export default function GroupBox(props: GroupBoxProps) {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={styles.title}>{props.title}</Text>
+      {props.children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 12,
+    paddingVertical: 18,
+    position: "relative",
+    borderRadius: 8
+  },
+  title: {
+    position: "absolute",
+    top: -12,
+    left: 12,
+    backgroundColor: "#f0f0f0",
+    paddingHorizontal: 4
+  }
+});
