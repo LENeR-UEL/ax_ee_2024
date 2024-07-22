4476e5f893345bedd991ec1e6ca716e6f91efdf2
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2024-07-22 09:43:43 -0300

    Corrigir avanço de estado após coleta de peso

--- gateway/src/states/04_ParallelWeight.cpp
+++ gateway/src/states/04_ParallelWeight.cpp
@@ -1,91 +1,77 @@
-#include <Arduino.h>
-#include <esp_log.h>
-#include <Preferences.h>
 #include "../Bluetooth/Bluetooth.h"
-#include "../Twai/Twai.h"
 #include "../Data.h"
-#include "../StateManager.h"
 #include "../Scale/Scale.h"
+#include "../StateManager.h"
+#include "../Twai/Twai.h"
+#include <Arduino.h>
+#include <Preferences.h>
+#include <esp_log.h>
 
 static const char *TAG = "ParallelWeight";
 static unsigned long lastTwaiSendTime = 0;
 
 static Preferences preferences;
 
-void loadStoredWeight()
-{
-    preferences.begin("parameters", true);
-    if (preferences.isKey("03weight"))
-    {
-        data.collectedWeight = preferences.getUShort("03weight", 0);
-    }
-    preferences.end();
+void loadStoredWeight() {
+  preferences.begin("parameters", true);
+  if (preferences.isKey("03weight")) {
+    data.collectedWeight = preferences.getUShort("03weight", 0);
+  }
+  preferences.end();
 }
 
-void storeWeight()
-{
-    preferences.begin("parameters", false);
-    preferences.putUShort("03weight", data.collectedWeight);
-    preferences.end();
+void storeWeight() {
+  preferences.begin("parameters", false);
+  preferences.putUShort("03weight", data.collectedWeight);
+  preferences.end();
 }
 
-void onParallelWeightStateEnter()
-{
-    loadStoredWeight();
+void onParallelWeightStateEnter() { loadStoredWeight(); }
+
+void onParallelWeightStateLoop() {
+  if (!espBle.isConnected()) {
+    stateManager.switchTo(StateKind::Disconnected);
+    return;
+  }
+
+  long now = millis();
+  if (now - lastTwaiSendTime >= 15) {
+    lastTwaiSendTime = now;
+    twaiSend(TwaiSendMessageKind::WeightTotal,
+             scaleGetWeightL() + scaleGetWeightR());
+    twaiSend(TwaiSendMessageKind::SetRequestedPwm, 0);
+    twaiSend(TwaiSendMessageKind::Setpoint, 0);
+    twaiSend(TwaiSendMessageKind::Mese, 0);
+    twaiSend(TwaiSendMessageKind::MeseMax, 0);
+    twaiSend(TwaiSendMessageKind::Trigger, (uint8_t)FlagTrigger::MalhaAberta);
+  }
 }
 
-void onParallelWeightStateLoop()
-{
-    if (!espBle.isConnected())
-    {
-        stateManager.switchTo(StateKind::Disconnected);
-        return;
-    }
-
-    long now = millis();
-    if (now - lastTwaiSendTime >= 15)
-    {
-        lastTwaiSendTime = now;
-        twaiSend(TwaiSendMessageKind::WeightTotal, scaleGetWeightL() + scaleGetWeightR());
-        twaiSend(TwaiSendMessageKind::SetRequestedPwm, 0);
-        twaiSend(TwaiSendMessageKind::Setpoint, 0);
-        twaiSend(TwaiSendMessageKind::Mese, 0);
-        twaiSend(TwaiSendMessageKind::MeseMax, 0);
-        twaiSend(TwaiSendMessageKind::Trigger, (uint8_t)FlagTrigger::MalhaAberta);
-    }
+void onParallelWeightStateTWAIMessage(TwaiReceivedMessage *receivedMessage) {
+  switch (receivedMessage->Kind) {
+  case TwaiReceivedMessageKind::PwmFeedbackEstimulador:
+    data.pwmFeedback = receivedMessage->ExtraData;
+    break;
+  }
 }
 
-void onParallelWeightStateTWAIMessage(TwaiReceivedMessage *receivedMessage)
-{
-    switch (receivedMessage->Kind)
-    {
-    case TwaiReceivedMessageKind::PwmFeedbackEstimulador:
-        data.pwmFeedback = receivedMessage->ExtraData;
-        break;
-    }
+void onParallelWeightStateBLEControl(BluetoothControlCode code,
+                                     uint8_t extraData) {
+  switch (code) {
+  case BluetoothControlCode::Parallel_RegisterWeight:
+    ESP_LOGI(TAG, "Peso corporal coletado");
+    data.collectedWeight = data.weightL + data.weightR;
+    return;
+  case BluetoothControlCode::Parallel_Complete:
+    stateManager.switchTo(StateKind::OperationStart);
+    return;
+  case BluetoothControlCode::Parallel_GoBackToMESECollecter:
+    stateManager.switchTo(StateKind::MESECollecter);
+    return;
+  case BluetoothControlCode::FirmwareInvokeReset:
+    esp_restart();
+    return;
+  }
 }
 
-void onParallelWeightStateBLEControl(BluetoothControlCode code, uint8_t extraData)
-{
-    switch (code)
-    {
-    case BluetoothControlCode::Parallel_RegisterWeight:
-        ESP_LOGI(TAG, "Peso corporal coletado");
-        data.collectedWeight = data.weightL + data.weightR;
-        return;
-    case BluetoothControlCode::Parallel_Complete:
-        stateManager.switchTo(StateKind::OperationMalhaFechada);
-        return;
-    case BluetoothControlCode::Parallel_GoBackToMESECollecter:
-        stateManager.switchTo(StateKind::MESECollecter);
-        return;
-    case BluetoothControlCode::FirmwareInvokeReset:
-        esp_restart();
-        return;
-    }
-}
-
-void onParallelWeightStateExit()
-{
-    storeWeight();
-}
+void onParallelWeightStateExit() { storeWeight(); }
